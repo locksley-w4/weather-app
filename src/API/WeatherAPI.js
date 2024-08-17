6853d77bbe16c698f2592855109db283003e3971
Locksley <94060212ad@gmail.com>
2024-08-17 12:47:57 +0500

    build fix2

--- src/API/WeatherAPI.js
+++ src/API/WeatherAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 class WeatherAPI {
     static getKey = () => "e394b4fd8d0e4c4a8f8101523241208";
     static async getWeather(params) {
-        const res = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${WeatherAPI.getKey()}&q=${params.query || "Tashkent"}&lang=${params.lang || "ru"}&days=${params.days || 5}`);
+        const res = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${WeatherAPI.getKey()}&q=${params.query || "Tashkent"}&lang=${params.lang || "ru"}&days=${params.days || 5}`);
         return res.data
     }
 }
